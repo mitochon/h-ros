928600fb3e15d5ff90bfb76ff395aab9883c61b1
Francisco Tanudjaja <francisco@bina.com>
2015-09-21 13:23:15 -0700

    fix gc.hs

--- src/gc.hs
+++ src/gc.hs
@@ -2,18 +2,23 @@ import           Control.Monad (liftM2)
 
 -- | Given DNA strings in fasta format, return the ID of the string having
 -- the highest GC content
-
 parseGC :: [String] -> (String,String) -> (String,Float) -> (String,Float)
-parseGC []     _        p = p
-parseGC (x:xs) (id,seq) p = case x of
-  ('>':nId) -> let p'  = (id, gcContent seq)
-                   max = if (snd p' > snd p) then p' else p
-               in parseGC xs (nId, "") max
+parseGC []     q          p = getMax q p
+parseGC (x:xs) q@(id,seq) p = case x of
+  ('>':nId) -> parseGC xs (nId, "") (getMax q p)
   nSeq      -> parseGC xs (id, seq ++ nSeq) p
+
+
+-- | evaluates the first tuple and compares it against the second tuple
+-- and return the tuple with the maximum associated value
+getMax :: (String,String) -> (String,Float) -> (String,Float)
+getMax (id,seq) p = let p'  = (id, gcContent seq)
+                        max = if (snd p' > snd p) then p' else p
+                    in max
+
 
 -- | calculates the GC-content of a DNA string, defined as the number of
 -- 'G' and 'C' bases over the length of the sequence
-
 gcContent :: String -> Float
 gcContect []    = 0
 gcContent seq   = gcLen / seqLen * 100
