2d192447d0a186437b624f33e58b5d3099ecda14
Francisco Tanudjaja <francisco@bina.com>
2015-09-26 15:49:03 -0700

    subs

---
+++ src/subs.hs
@@ -0,0 +1,22 @@
+-- Given 2 DNA strings s and t return all locations of t as a substring of s
+--
+-- >>> GATATATGCATATACTT
+-- >>> ATAT
+-- 2 4 10
+
+subs :: String -> String -> [Bool]
+subs [] _ = []
+subs _ [] = []
+subs s t  =
+  let (m,_) = splitAt (length t) s
+      n     = subs (tail s) t
+  in if (m == t) then (True:n) else (False:n)
+
+
+main = do
+  s <- getLine
+  t <- getLine
+  let u = subs s t      -- [u1,u2,...]
+      v = zip u [1..]   -- [(u1,1), (u2,2), ...]
+      w = filter fst v  -- filter for u(n) = True
+  print $ map snd w
